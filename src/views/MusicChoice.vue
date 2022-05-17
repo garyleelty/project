27814308ec65421c07de7062cd229e1c03f3de5a
garyleelty <tl3285@nyu.edu>
2022-05-17 18:54:02 -0400

    comit

--- src/views/MusicChoice.vue
+++ src/views/MusicChoice.vue
@@ -1,17 +1,42 @@
 <template>
-  <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/6cfVDaIdvDtYH91RqC6Wox?utm_source=generator" width="50%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
-  <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/0ml8EENbogTBv1oB7RJHY2?utm_source=generator" width="50%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+  <div v-for="(url1) in url">
+    <iframe :src="url1" width="50%" height="80" frameBorder="0" allow="autoplay; encrypted-media;"></iframe>
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
 export default {
+  data(){
+    return{
+      uri:[],
+      url:[],
+    };
+  },
+  beforeMount(){
+    axios.get('https://project1-b1937-default-rtdb.firebaseio.com/users/.json')
+      .then((res) => {
+        var a= Object.values(res)[0]
+        this.uri = Object.values(a)[0]
+        console.log(this.uri)
+        for (let x of this. uri) {
+          this.url.push('https://open.spotify.com/embed/track/'+x+'?utm_source=generator');
+        }
+
+
+      })
+      .catch(err => console.log(err))
+    
+    
+  },
   methods:{
     get(){
       axios.get('https://project1-b1937-default-rtdb.firebaseio.com/users/.json')
-      .then((res) => console.log(res))
+      .then((res) => {
+        console.log(res)
+      })
       .catch(err => console.log(err))
-    }
+    },
   }
 }
 </script>
